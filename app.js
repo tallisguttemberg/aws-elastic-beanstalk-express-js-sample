0daeb84bbcb3169269266623f16739aaa14e86af
Tallis Guttemberg <tallysguimaraes@gmail.com>
2021-12-22 06:33:39 -0300

    TESTE COMPLETO PIPELINE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('VOCÊ VAI CONSEGUIR!'));
+app.get('/', (req, res) => res.send('ESSA AWS É VIOLENTA'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
